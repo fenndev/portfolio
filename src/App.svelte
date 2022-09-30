247b92ab4653b3284a1a09549441ce3956aa9aa5
fenndev <fenndev@protonmail.com>
2022-09-30 01:36:01 -0700

    Began styling main page

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,8 @@
 <script lang='ts'>
-    
+    import Intro from './Svelte/Intro.svelte';
 </script>
 
-<h1>Hello World</h1>
+<Intro />
 
 <style lang="scss">
-    @import './global.scss';
-    h1 {
-        color: red;
-        font-family: $font-main;
-        
-    }
 </style>
---
+++ src/Svelte/Intro.svelte
@@ -0,0 +1,28 @@
+<h1><span id="dollar">$</span>: fenndev --info</h1>
+<h2>Hi, my name's Fenn.</h2>
+<h3>I'm a full-stack software developer with a passion for learning and
+    continuous improvement.<span id="blink">_</span>
+</h3>
+
+<style lang="scss">
+    @import '../global.scss';
+
+    h1 {
+        font-family: $font-main-bold;
+    }
+    h2, h3 {
+        font-family: $font-main;
+    }
+
+    #dollar {
+        color: $color-mint;
+    }
+
+    #blink {
+        animation: blink 0.5s infinite;
+    }
+    @keyframes blink {
+        0%, 100% { opacity: 0; }
+        50% { opacity: 1; }
+    }
+</style>

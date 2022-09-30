cc150446059c07cf43a3589cdd653c933b664ee6
fenndev <fenndev@protonmail.com>
2022-09-30 01:06:34 -0700

    Adjusted icons

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,14 @@
+<script lang='ts'>
+    
+</script>
+
 <h1>Hello World</h1>
 
 <style lang="scss">
+    @import './global.scss';
     h1 {
         color: red;
+        font-family: $font-main;
+        
     }
 </style>

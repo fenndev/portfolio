274b21718965109aa6195e4bea97aa895d277969
fenndev <fenndev@protonmail.com>
2022-10-03 00:29:54 -0700

    Completed placeholder descriptions

--- src/Svelte/Pages/Work.svelte
+++ src/Svelte/Pages/Work.svelte
@@ -28,40 +28,40 @@
         <a href="http://">GitHub</a>
     </article>
     <article id='une'>
-        <img src="" alt="A preview of the UNE homepage.">
+        <img src="" alt="A preview of the weather.ly app.">
         <div>
-            <h3>United Nations of Earth</h3>
-            <p>A webpage for the United Nations of Earth, a fictional government from the video game Stellaris. Made following the <a href="https://www.un.org/styleguide">United Nations web style guide.</a></p>
+            <h3>weather.ly</h3>
+            <p>A web-based weather application.</p>
         </div>
         <a href="http://">Live Site</a>
         <a href="http://">GitHub</a>
     </article>
-    <article id='une'>
-        <img src="" alt="A preview of the UNE homepage.">
+    <article id='etch-a-sketch'>
+        <img src="" alt="A preview of my Etch-a-Sketch application.">
         <div>
-            <h3>United Nations of Earth</h3>
-            <p>A webpage for the United Nations of Earth, a fictional government from the video game Stellaris. Made following the <a href="https://www.un.org/styleguide">United Nations web style guide.</a></p>
+            <h3>Etch-a-Sketch</h3>
+            <p>An Etch-a-Sketch application written in vanilla HTML, CSS, and Javascript.</p>
         </div>
-        <a href="http://">Live Site</a>
-        <a href="http://">GitHub</a>
+        <a href="https://fenndev.github.io/etch-a-sketch/">Live Site</a>
+        <a href="https://github.com/fenndev/etch-a-sketch">GitHub</a>
     </article>
-    <article id='une'>
-        <img src="" alt="A preview of the UNE homepage.">
+    <article id='dovey'>
+        <img src="" alt="A preview of a website made for a Twitch streamer.">
         <div>
-            <h3>United Nations of Earth</h3>
-            <p>A webpage for the United Nations of Earth, a fictional government from the video game Stellaris. Made following the <a href="https://www.un.org/styleguide">United Nations web style guide.</a></p>
+            <h3>Clockwork Dovey</h3>
+            <p>A webpage made for a Twitch streamer.</p>
         </div>
-        <a href="http://">Live Site</a>
-        <a href="http://">GitHub</a>
+        <a href="https://fenndev.github.io/dovey-site/">Live Site</a>
+        <a href="https://github.com/fenndev/dovey-site">GitHub</a>
     </article>
-    <article id='une'>
-        <img src="" alt="A preview of the UNE homepage.">
+    <article id='strange'>
+        <img src="" alt="A preview of an online card game site.">
         <div>
-            <h3>United Nations of Earth</h3>
-            <p>A webpage for the United Nations of Earth, a fictional government from the video game Stellaris. Made following the <a href="https://www.un.org/styleguide">United Nations web style guide.</a></p>
+            <h3>Strangers: The Card Game</h3>
+            <p>A website hosting a card game that I designed and programmed, with Stable Diffusion and Midjourney helping to create the illustrations.</p>
         </div>
         <a href="http://">Live Site</a>
-        <a href="http://">GitHub</a>
+        <a href="https://github.com/fenndev/card-test">GitHub</a>
     </article>
 </section>
 
@@ -85,8 +85,9 @@
 
     section {
         display: grid;
-        grid-template-columns: repeat(auto-fit, minmax(max(33%, 15rem), 1fr));
+        grid-template-columns: repeat(auto-fit, minmax(max(25%, 10rem), 1fr));
         justify-items: center;
         row-gap: 4rem;
+        column-gap: 4rem;
     }
 </style>
